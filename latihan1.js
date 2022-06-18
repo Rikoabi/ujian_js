dedcdd004ef18a0792fb0428a03e9d3c0d3ecfc7
Rikoabi <93429793+Rikoabi@users.noreply.github.com>
2022-06-18 11:21:51 +0700

    Add files via upload
    Ujian KafeKoding no 1 dan 2

---
+++ latihan1.js
@@ -0,0 +1,16 @@
+const tempat = 'bogor';
+const karna = 'ini adalah tempat yang paling ingin dikunjungi'
+
+if (tempat === 'bajo'){
+   karna === 'ini adalah tempat yang paling ingin dikunjungi';
+}else if (tempat === 'bali'){
+    karna==='tempat yang ingin di kunjungi setelah nya'
+}else if (tempat === 'raja ampat'){
+    karna==='tempat yang sangat indah'
+}else if (tempat ==='jakarta'){
+    karna==='metropolitan'
+}else{
+    console.log('tempat yang anda cari tidak ada ')
+}
+
+console.log (tempat)
